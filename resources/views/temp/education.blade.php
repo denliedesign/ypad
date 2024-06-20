6fe3ab012c2e392c4cc079c641a469520167299e
denliedesign <customdenlie@gmail.com>
2024-06-19 22:15:32 -0500

    sizing gift

--- resources/views/temp/education.blade.php
+++ resources/views/temp/education.blade.php
@@ -40,9 +40,9 @@
         @include('_divider-bottom')
         <div class="container d-flex justify-content-center align-items-center py-5">
             <div class="text-white pt-5">
-                <div class="text-center"><ion-icon name="gift" style="font-size: 3em;"></ion-icon></div>
+                <div class="text-center"><ion-icon name="gift" style="font-size: 4em;"></ion-icon></div>
                 <h2 class="text-uppercase text-center">buy one, give one</h2>
-                <p class="text-center">For every course that is purchased, a scholarship course is made available to another member of the dance community.</p>
+                <p class="text-center" style="font-size: 22px;">For every course that is purchased...<br>a scholarship course is made available to another member of the dance community!</p>
 
                 {{--                <h3 class="text-white text-center pt-4">--}}
 {{--                    The mission of YPAD™ is to bring awareness to and provide guidance for one essential priority: <strong>the safety and health of dance students</strong>.--}}
